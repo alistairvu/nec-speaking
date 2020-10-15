88e2168ad7cddc3ce7d808186409b2a5958d47df
Alistair Vu <alistairvu@gmail.com>
2020-10-15 23:21:13 +0700

    Fix a typo

--- data.js
+++ data.js
@@ -7,6 +7,6 @@ export const prompts = [
   "Some people think that children should be educated to be honest before anything else. Others believe that honesty is only one quality and children can develop it by themselves. Discuss both views and give your opinion. (NEC 2020)",
   "Some people believe that a decision should be changed when circumstances change. Others that the reverse is true. Discuss both views and give your opinion. (NEC 2020)",
   "Some people believe that discipline hinders creativity. Others think that creativity without discipline will not bring success. Discuss both views and give your opinion. (NEC 2020)",
-  "Some people think that it is unacceptable for social networking sites to sell pur personal information. Others believe it is the trade-off we must make in order to maintain our online presence. Discuss both views and give your opinion. (NEC 2020)",
+  "Some people think that it is unacceptable for social networking sites to sell our personal information. Others believe it is the trade-off we must make in order to maintain our online presence. Discuss both views and give your opinion. (NEC 2020)",
   "Some people believe that it is a good idea for parents to put constant pressure on their kids to do well. Others think that such pressure from parents is detrimental to their children's development. Discuss both views and give your opinion. (NEC 2020)",
 ];
